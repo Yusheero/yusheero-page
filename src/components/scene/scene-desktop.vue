5d860155b04ba5e663eb08e22b8823b694928b54
Yusheero <yusheero.dev@gmail.com>
2025-01-21 21:00:32 +1000

    hotfix scene

--- src/components/scene/scene-desktop.vue
+++ src/components/scene/scene-desktop.vue
@@ -5,58 +5,51 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import modelPath from '@/assets/models/trueno/toyota_ae86.glb';
 
-/** Флаг отображения сцены с моделью */
-const useScene = true;
+const canvasContainer = useTemplateRef('canvasContainer');
+const initThreeJS = () => {
+  // Создаем сцену
+  const scene = new THREE.Scene();
 
-if (useScene) {
-  const canvasContainer = useTemplateRef('canvasContainer');
-  const initThreeJS = () => {
-    // Создаем сцену
-    const scene = new THREE.Scene();
+  // Создаем камеру
+  const camera = new THREE.PerspectiveCamera(75, canvasContainer.value.offsetWidth / canvasContainer.value.offsetHeight, 0.1, 1000);
+  camera.position.set(3, 1, 1);
 
-    // Создаем камеру
-    const camera = new THREE.PerspectiveCamera(75, canvasContainer.value.offsetWidth / canvasContainer.value.offsetHeight, 0.1, 1000);
-    camera.position.set(3, 1, 1);
+  // Настройка рендерера
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(canvasContainer.value.offsetWidth, canvasContainer.value.offsetHeight);
+  canvasContainer.value.appendChild(renderer.domElement);
 
-    // Настройка рендерера
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(canvasContainer.value.offsetWidth, canvasContainer.value.offsetHeight);
-    canvasContainer.value.appendChild(renderer.domElement);
+  // Добавляем освещение
+  const light = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(5, 5, 5).normalize();
+  scene.add(directionalLight);
 
-    // Добавляем освещение
-    const light = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(light);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 5, 5).normalize();
-    scene.add(directionalLight);
+  // Загружаем модель
+  const loader = new GLTFLoader();
+  loader.load(modelPath, function ( gltf ) {
+    scene.add(gltf.scene);
+    console.error('Всё по кайфу');
+  }, undefined, function (error) {
+    console.error(error);
+  });
 
-    // Загружаем модель
-    const loader = new GLTFLoader();
-    loader.load(modelPath, function ( gltf ) {
-      scene.add(gltf.scene);
-      console.error('Всё по кайфу');
-    }, undefined, function (error) {
-      console.error(error);
-    });
-
-    // Добавляем OrbitControls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true; // Включаем затухание
-    controls.dampingFactor = 0.25; // Фактор затухания
-    controls.screenSpacePanning = false; // Запрет панорамирования в пространстве экрана
-    function animate() {
-      requestAnimationFrame(animate)
-      renderer.render(scene, camera);
-    }
-    animate();
-  };
-}
-
+  // Добавляем OrbitControls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true; // Включаем затухание
+  controls.dampingFactor = 0.25; // Фактор затухания
+  controls.screenSpacePanning = false; // Запрет панорамирования в пространстве экрана
+  function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera);
+  }
+  animate();
+};
 
 onMounted(() => {
-  if (useScene) {
-    initThreeJS();
-  }
+  initThreeJS();
+
 });
 </script>
 <template>
