d6c6be18545c815d3b9270bbe7c02f631d47e35e
Yusheero <yusheero.dev@gmail.com>
2025-01-21 17:34:37 +1000

    add threejs model

--- src/components/scene/scene-desktop.vue
+++ src/components/scene/scene-desktop.vue
@@ -1,42 +1,50 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { onMounted, useTemplateRef } from 'vue';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-// import modelPath from '@/assets/models/trueno/toyota_ae86.glb';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import modelPath from '@/assets/models/trueno/toyota_ae86.glb';
 
-const canvasContainer = ref();
-let scene, camera, renderer, controls;
-let cube;
-
+const canvasContainer = useTemplateRef('canvasContainer');
 const initThreeJS = () => {
   // Создаем сцену
-  scene = new THREE.Scene();
-
-  console.log('canvasContainer.offsetHeight', canvasContainer.value.offsetHeight);
-  console.log('canvasContainer.offsetWidth', canvasContainer.value.offsetWidth);
+  const scene = new THREE.Scene();
 
   // Создаем камеру
-  camera = new THREE.PerspectiveCamera(75, canvasContainer.value.offsetWidth / canvasContainer.value.offsetHeight, 0.1, 1000);
-  camera.position.set(0, 0, 5);
+  const camera = new THREE.PerspectiveCamera(75, canvasContainer.value.offsetWidth / canvasContainer.value.offsetHeight, 0.1, 1000);
+  camera.position.set(3, 1, 1);
 
   // Настройка рендерера
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(canvasContainer.value.offsetWidth, canvasContainer.value.offsetHeight);
   canvasContainer.value.appendChild(renderer.domElement);
 
+  // Добавляем освещение
+  const light = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(5, 5, 5).normalize();
+  scene.add(directionalLight);
 
-
-  // Создаем куб
-  const geometry = new THREE.BoxGeometry(2, 2, 2);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  // Загружаем модель
+  const loader = new GLTFLoader();
+  loader.load(modelPath, function ( gltf ) {
+    scene.add(gltf.scene);
+    console.error('Всё по кайфу');
+  }, undefined, function (error) {
+	  console.error(error);
+  });
 
   // Добавляем OrbitControls
-  controls = new OrbitControls(camera, renderer.domElement);
+  const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true; // Включаем затухание
   controls.dampingFactor = 0.25; // Фактор затухания
   controls.screenSpacePanning = false; // Запрет панорамирования в пространстве экрана
+  function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera);
+  }
+  animate();
 };
 
 onMounted(() => {
