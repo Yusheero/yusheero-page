38591d6c806cc9d5bf326a2959cebdf119477c88
Yusheero <yusheero.dev@gmail.com>
2025-01-22 13:00:21 +1000

    library refactoring

--- src/views/library-view-desktop.vue
+++ src/views/library-view-desktop.vue
@@ -2,6 +2,7 @@
 import Navigation from '@/components/navigation/navigation.vue';
 import ViewsTemplateDesktop from '@/components/views-template/views-template-desktop.vue';
 import data from '@/data/library.json';
+import { SquareArrowOutUpRight } from 'lucide-vue-next';
 
 const libraryData = data;
 console.log('libraryData', libraryData);
@@ -10,14 +11,21 @@ console.log('libraryData', libraryData);
 
 <template>
   <ViewsTemplateDesktop>
-    <div class="game-view-desktop">
-      <Navigation class="game-view-desktop__navigation" />
-      <div class="game-view-desktop__title">Library Page</div>
-      <div class="game-view-desktop__content content">
+    <div class="library-view-desktop">
+      <Navigation class="library-view-desktop__navigation" />
+      <div class="library-view-desktop__title">Library Page</div>
+      <div class="library-view-desktop__content content">
         <div class="content__sections">
-          <div v-for="category in libraryData" class="content__sections-item section">
+          <div v-for="(category, index) in libraryData" class="content__sections-item section">
+            <div class="section__title">
+              {{ index }}
+            </div>
             <div class="section__item" v-for="data in category">
-              {{ data.title }}
+              <p class="section__item-title">{{ data.title }}</p>
+              <div class="section__item-info">
+                <p v-if="data.genre" class="section__item-genre">{{ data.genre }}</p>
+                <a v-if="data.link" class="section__item-link" :href="data.link"><SquareArrowOutUpRight color="black" size="18" stroke-width="1.5" /></a>
+              </div>
             </div>
           </div>
         </div>
@@ -29,7 +37,7 @@ console.log('libraryData', libraryData);
 <style lang="scss">
 @import '@/assets/style/style.scss';
 
-.game-view-desktop {
+.library-view-desktop {
   height: 100%;
   width: 100%;
   color: var(--color-secondary);
@@ -94,23 +102,67 @@ console.log('libraryData', libraryData);
     // TODO:Убрать хардкод у height
     height: 100%;
     width: 25%;
-    background: var(--color-orange);
+    background: var(--color-secondary);
     border-radius: 6px;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-    gap: 12px;
+    gap: 4px;
   }
 }
 
 .section {
+  &__title {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 32px;
+    color: var(--color-primary);
+  }
+
   &__item {
-    background: var(--color-primary);
-    padding: 8px;
-    border-radius: 8px;
+    height: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    font-size: 16px;
+    background: var(--color-orange);
+    color: var(--color-primary);
+    padding: 8px 8px;
+    border-radius: 6px;
     width: 100%;
     text-align: center;
+    border: 1px solid var(--color-primary);
+  }
+
+  &__item-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+  }
+
+  &__item-genre {
+    width: 60px;
+    background: var(--color-secondary);
+    padding: 4px;
+    border-radius: 4px;
+    font-size: 12px;
+    font-weight: 400;
+  }
+
+  &__item-link {
+    background: var(--color-secondary);
+    padding: 2px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
   }
 }
 </style>
